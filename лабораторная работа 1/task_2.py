3e0b1110eff10c43b419c5e29df82670b7ad6b92
aerisssa <ekaterina221005@gmail.com>
2024-12-09 17:32:58 +0300

    Лабораторная работа 1

--- лабораторная работа 1/task_2.py
+++ лабораторная работа 1/task_2.py
@@ -6,22 +6,24 @@ if __name__ == "__main__":
     fridge = Refrigerator(temperature=4, capacity=200)
     phone = Phone(brand="Samsung", battery_capacity=4000)
     kettle = ElectricKettle(volume=1.5)
-# TODO: инстанцировать все описанные классы, создав три объекта.C()
+    # TODO: инстанцировать все описанные классы, создав три объекта.
 
     try:
         print(fridge.adjust_temperature(-40))
-# TODO: вызвать метод с некорректными аргументами(b)
+        # TODO: вызвать метод с некорректными аргументами(b)
     except ValueError:
         print('Ошибка: неправильные данные')
 
     try:
         print(phone.make_call(-5))
-# TODO: вызвать метод с некорректными аргументами(a)
+        # TODO: вызвать метод с некорректными аргументами(a)
     except ValueError:
         print('Ошибка: неправильные данные')
 
     try:
         print(kettle.fill(3))
-# TODO: вызвать метод с некорректными аргументами(a)
+        # TODO: вызвать метод с некорректными аргументами(a)
     except ValueError:
         print('Ошибка: неправильные данные')
+
+
